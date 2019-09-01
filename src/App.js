8d8e887553c86693b323bfbd000f5f89c71becb7
mohammad-talib <mohammad.h.alhety@gmail.com>
2019-09-01 09:18:52 +0300

    mini-proj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state={
+    user:{
+      useravatar:'',
+      followers:'',
+      following:'',
+      public_repos:''
+    }
+  }
+
+  getName = () =>{
+    var val = this.refs.val.value;
+    axios.get(`http://api.github.com/users/${val}`)
+    .then(res => {
+      this.setState({
+        user:{
+        useravatar:res.data.avatar_url,
+        followers:res.data.followers ,
+        following:res.data.following,
+        public_repos:res.data.public_repos
+        }
+      });
+      console.log()
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render() { 
+    const {user} = this.state;
+    return (
+      <div className="container">
+       <div className="App">
+         <input type="text" placeholder="Search For User" ref="val"/>
+         <button onClick={this.getName}>Search</button>
+         </div> 
+         <div className="grid">
+            <div className="img"><img src={user.useravatar} width="200px" alt={user.useravatar}/></div>
+            <div>Repositories: {user.public_repos}</div>
+            <div>Followers: {user.followers}</div>
+            <div>Following: {user.following}</div>
+         </div>
+      </div>
+      );
+  }
 }
-
+ 
 export default App;
